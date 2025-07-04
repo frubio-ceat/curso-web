edf09b80ef45d7b4c2987ec2e363544683ed0331
ferrubio <fernando.rp@vvision.eu>
2025-07-04 18:08:05 +0200

    imports

---
+++ Despliegue/02_imports/main.js
@@ -0,0 +1,4 @@
+import {MI_CONSTANTE, saludar} from './mi_script_01.js';
+
+console.log(MI_CONSTANTE);
+saludar();
---
+++ Despliegue/02_imports/mi_script_01.js
@@ -0,0 +1,6 @@
+
+export const MI_CONSTANTE = "Variable de ejemplo";
+
+export function saludar() {
+  console.log("¡Hola desde mi-script-01.js!");
+}
